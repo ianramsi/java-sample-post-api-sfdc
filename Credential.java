d94186768cb5eb218f2e5643bbdf6ab4bcad0036
ianramsi <ian.ramsianto@gmail.com>
2020-05-29 14:44:09 +0700

    sample post to sfdc

---
+++ Credential.java
@@ -0,0 +1,30 @@
+package STC;
+
+public class Credential {
+
+    private static final String userName = "";
+    private static final String password = "";
+    private static final String clientSec = "";
+    private static final String clientKey = "";
+    private static final String urlLogin = "";
+
+    public static String getUrlLogin(){
+        return urlLogin;
+    }
+
+    public static String getUserName(){
+        return userName;
+    }
+
+    public static String getPassword(){
+        return password;
+    }
+
+    public static String getClientSec(){
+        return clientSec;
+    }
+
+    public static String getClientKey(){
+        return clientKey;
+    }
+}
